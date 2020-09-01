38a0b8bfa9f2338fa6bb572ee98d3d108fde0bab
@gmgower <gmgower@gmail.com>
2020-08-31 19:09:19 -0500

    page setup component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './App.css';
+import Error from './pages/Error';
+import Rooms from './pages/Rooms';
+import Home from './pages/Home';
+import SingleRoom from './pages/SingleRoom';
+
 
 function App() {
   return (
-    <>
-    hello
-    </>
+    <div>
+    App page
+    <Home />
+    <Rooms />
+    <SingleRoom />
+    <Error />
+    </div>
   );
 }
 
