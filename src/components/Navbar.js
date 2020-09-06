ffdc0608933bfaa790182c286531d9c92f57cf01
@gmgower <gmgower@gmail.com>
2020-09-06 11:07:29 -0500

    Style Navbar component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,13 +27,15 @@ export default class Navbar extends Component {
               <FaAlignRight className="nav-icon" />
             </button>
           </div>
-          <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
-              <li>
-                  <Link to="/">Home</Link>
-              </li>
-              <li>
-                  <Link to="/rooms">Rooms</Link>
-              </li>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
           </ul>
         </div>
       </nav>
