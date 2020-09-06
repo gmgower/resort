ffa9bd48b07d2e2acda7121f518289d8e75d552c
@gmgower <gmgower@gmail.com>
2020-09-06 10:06:31 -0500

    Navbar setup componenet.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import SingleRoom from "./pages/SingleRoom";
 
 import { Route, Switch } from "react-router-dom";
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
+    <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
